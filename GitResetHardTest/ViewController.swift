c373ff2aa579dceea32537d6c0c7b5012442c6ff
Lekhnish Jha <lekhnishcusat@gmail.com>
2019-01-11 17:02:56 +0530

    Initial codes

---
+++ GitResetHardTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitResetHardTest
+//
+//  Created by lekhnish jha on 11/01/19.
+//  Copyright © 2019 lekhnish jha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
